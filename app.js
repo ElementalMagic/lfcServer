a5e21407f2cfd6619ae3eabeb353e8ba93cac578
Alex Vahtin <vkstrfrt@gmail.com>
2019-03-18 11:18:08 +0400

    v. 0.9

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var loginRouter = require('./routes/login');
 var reviewRouter = require('./routes/review');
 var multer = require('multer');
 var keys = require('./config/keys.js');
+var fs = require('fs');
 var app = express();
 
 mongoose
@@ -21,11 +22,11 @@ app.use(compression());
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.get('*', function (req,res,next) {
-    if(req.hostname === 'fabrikabloknotov.ru' || !process.env.CHECKDOMAIN){
+app.get('*', function (req, res, next) {
+    if (req.hostname === 'fabrikabloknotov.ru' || !process.env.CHECKDOMAIN) {
         next();
     } else {
         res.status(301).redirect('https://fabrikabloknotov.ru');
@@ -33,20 +34,20 @@ app.get('*', function (req,res,next) {
 });
 
 
-app.get('/credit', (req,res) => {
+app.get('/credit', (req, res) => {
     res.status(200).sendFile(path.resolve('../lfc/credit.html'))
 });
-app.get('/AndrewAlexTheBroysTheBank', function (req,res) {
+app.get('/AndrewAlexTheBroysTheBank', function (req, res) {
     console.log(req.query);
-    if(req.query.key === keys.secretKey){
+    if (req.query.key === keys.secretKey) {
         res.status(200).sendFile(path.resolve('../lfc/admin.html'));
     } else {
         res.redirect('/login');
     }
 });
 
-app.get('*', function (req,res,next) {
-    if(req.path.endsWith('.html')){
+app.get('*', function (req, res, next) {
+    if (req.path.endsWith('.html')) {
         res.sendFile(path.resolve('../lfc/NotFound.html'))
     } else {
         next();
@@ -54,17 +55,38 @@ app.get('*', function (req,res,next) {
 });
 
 app.use(express.static(path.resolve('../lfc')));
-app.use("/images", express.static("images"));
+app.get("/images/:id", function (req, res, next) {
+    let find = false;
+    let id = req.params.id;
+    fs.readdir(path.resolve('images'), function (err, items) {
+        items.forEach(function (item) {
+            if (item === id) {
+                console.log(path.resolve(__dirname + '/images/' + id));
+                find = true;
+                res.sendFile(path.resolve(__dirname + '/images/' + id));
+            }
+        });
+        if (!find) {
+            next();
+        }
+    });
+
+});
 app.use("/docs", express.static("docs"));
 
 app.use('/api/order', orderRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/review', reviewRouter);
-app.use('/', (req,res, next) => {
-    if(req.path != "/"){
+app.use('/', (req, res, next) => {
+    if (req.path != "/") {
         next();
     }
     res.status(200).sendFile(path.resolve('../lfc/index.html'));
 });
-app.use('*', (req,res) => res.status(404).sendFile(path.resolve('../lfc/NotFound.html')));
+app.use('/404', (req, res) => {
+    res.status(404).sendFile(path.resolve('../lfc/NotFound.html'))
+});
+app.use('*', (req, res) => {
+    res.status(404).redirect('http://localhost/404');
+});
 module.exports = app;
