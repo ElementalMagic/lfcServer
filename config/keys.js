aff0f5c9b5e060b4d54d42cc8c12bc4fc3d6d2e8
Alex Vahtin <vkstrfrt@gmail.com>
2019-03-25 12:26:41 +0300

    edit config [2]

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     secretKey: 'nhbnjy',
     mongoURI: 'mongodb://localhost:27017',
-    emailReceiver: 'vkstrfrt@gmail.com',
+    emailReceiver: 'info@lenfinance.ru',
     emailLogin: 'iqlex1'
 };
